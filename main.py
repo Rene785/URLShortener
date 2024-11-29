49e71b2f3722379a1bf65d092f1b91fd2e3f1ad8
Rene785 <rene.greff@arcor.de>
2024-11-29 02:55:49 +0100

    Logic implemented to shorten a link or to expand a bitlink.

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import requests
+import sys
 
 headers = {
     'Authorization': 'Bearer 1479c9a21b29e5b43598380543093a80ec768b09',
     'Content-Type': 'application/json',
 }
+print("Allowed expressions:\n")
+print("     shorten urlToShorten")
+print("     expand bitlinkToExpand\n")
 
-data = '{ "long_url": "https://www.youtube.com", "domain": "bit.ly"}'
+expression = input("Enter your command:")
+expression = expression.split(" ")
 
-response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+if expression.__len__ is not 2:
+    print("Invalid Expression. Program only accepts expression of the type 'command link'.")
+    sys.exit("Exiting program")
+
+command = expression[0]
+link = expression[1]
+
+if not command.startswith("shorten") or not command.startswith("expand"):
+    print("Invalid command: " + command[0])
+    sys.exit("Exiting program")
+
+if command.startswith("shorten"):
+    data = '{"long_url": "'+ link + '"}'
+    endpoint = 'https://api-ssl.bitly.com/v4/shorten'
+
+if command.startswith("expand"):
+    data = '{"bitlink_id": "'+ link + '"}'
+    endpoint = 'https://api-ssl.bitly.com/v4/expand'
+
+response = requests.post(endpoint, headers=headers, data=data)
 
 print(response.text)
